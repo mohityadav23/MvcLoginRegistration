94ec1eb13f91f6a342646e520b1463846c9dfa61
Mohit Yadav <Mohit.yadav@BCSTECH.IND>
2018-06-21 11:49:11 +0530

    Add project files.

---
+++ MvcLoginRegistration/Models/UserAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLoginRegistration.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Email is required.")]
+        //[RegularExpression("^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$", ErrorMessage ="Please Enter a valid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "UserName is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
